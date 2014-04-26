2013ac2d4a07d9b4e0aed4510cece1345a972832
shine911 <deathbook1999@gmail.com>
2014-04-26 11:55:27 +0700

    AOSPA not need

--- patch.sh
+++ patch.sh
@@ -8,6 +8,7 @@ cd native
 git fetch https://github.com/shine911/AOSPA_frameworks_native kitkat
 git cherry-pick 083373c8ee1257997320dc2c9c0d041547a58d77
 git cherry-pick 125757e88966464fd1984818ff7666ac1cee9fb2
+
 echo -e $CL_BLU"Cherrypicking Legacy sensors"$CL_RST
 git fetch http://review.cyanogenmod.org/CyanogenMod/android_frameworks_native refs/changes/11/59311/1
 git cherry-pick FETCH_HEAD
@@ -31,11 +32,6 @@ cd hardware/libhardware_legacy
 git fetch https://github.com/TeamCanjica/android_hardware_libhardware_legacy cm-11.0
 git cherry-pick 9c2250d32a1eda9afe3b5cefe3306104148aa532
 cd ../..
-echo -e $CL_BLU"Cherrypicking Camera fix"$CL_RST
-cd packages/apps/Camera2
-git fetch https://github.com/CyanogenMod/android_packages_apps_Camera2 cm-11.0
-git cherry-pick 42067bbce2203088e09039169b0262691dd07e97
-cd ../../..
 echo -e $CL_BLU"Cherrypicking Low-incall Volume Fix"$CL_RST
 cd packages/services/Telephony
 git fetch https://github.com/shine911/packages_services_Telephony kk4.4
