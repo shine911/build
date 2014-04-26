4498f0173deec659f0fe0af51e3a365e3c6abe11
shine911 <deathbook1999@gmail.com>
2014-04-26 11:46:33 +0700

    AOSPA fixes

--- patch.sh
+++ patch.sh
@@ -1,12 +1,13 @@
 echo -e $CL_BLU"Cherrypicking OMX Patch - android_frameworks_av"$CL_RST
 cd frameworks/av
-git fetch https://github.com/shine911/frameworks_av kk4.4
-git cherry-pick 97b2d13620053eaa8b3425d3bbb486b2c4ef5a9f
+git fetch https://github.com/shine911/AOSPA_frameworks_av kitkat
+git cherry-pick 3234c927bfc388a55cd9e09cee36f3a3eb63fe1f
 cd ..
 echo -e $CL_BLU"Cherrypicking OMX Patch - android_frameworks_native"$CL_RST
 cd native
-git fetch https://github.com/shine911/frameworks_native kk4.4
-git cherry-pick e7c8482bf9e3287c81962ad573d8562995a388bc
+git fetch https://github.com/shine911/AOSPA_frameworks_native kitkat
+git cherry-pick 083373c8ee1257997320dc2c9c0d041547a58d77
+git cherry-pick 125757e88966464fd1984818ff7666ac1cee9fb2
 echo -e $CL_BLU"Cherrypicking Legacy sensors"$CL_RST
 git fetch http://review.cyanogenmod.org/CyanogenMod/android_frameworks_native refs/changes/11/59311/1
 git cherry-pick FETCH_HEAD
@@ -16,8 +17,9 @@ cp patch/patches/frameworks/base/core/jni/android_media_AudioRecord.cpp framewor
 echo -e $CL_BLU"Cherrypicking Core Patch - OMX, reboot/shutdown fix and samsung: allow lpm from command line"$CL_RST
 cp patch/patches/system/core/init/init.c system/core/init/init.c
 cd system/core
-git fetch https://github.com/shine911/android_system_core kk4.4
-git cherry-pick bef1a23e42c532b5f06fa54878434995478b1f54
+git fetch https://github.com/shine911/AOSPA_system_core kitkat
+git cherry-pick f39215e8ae4166cec8b02593b6e06b523d0e561c
+git cherry-pick 5e4096e52d2f3941006bf530038a147f561a38b4
 cd ..
 echo -e $CL_BLU"Cherrypicking vold patch to allow switching storages"$CL_RST
 cd vold
