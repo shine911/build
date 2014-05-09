b415555c0c275ead9cdb71913013b2ee52d422fb
shine911 <deathbook1999@gmail.com>
2014-05-09 17:58:50 +0700

    Big update omx fixes

--- patch.sh
+++ patch.sh
@@ -1,13 +1,12 @@
 echo -e $CL_BLU"Cherrypicking OMX Patch - android_frameworks_av"$CL_RST
 cd frameworks/av
 git fetch https://github.com/shine911/AOSPA_frameworks_av kitkat
-git cherry-pick 3234c927bfc388a55cd9e09cee36f3a3eb63fe1f
+git cherry-pick aee628ab06453665df02de409d762f618e59dedd
 cd ..
 echo -e $CL_BLU"Cherrypicking OMX Patch - android_frameworks_native"$CL_RST
 cd native
 git fetch https://github.com/shine911/AOSPA_frameworks_native kitkat
-git cherry-pick 083373c8ee1257997320dc2c9c0d041547a58d77
-git cherry-pick 125757e88966464fd1984818ff7666ac1cee9fb2
+git cherry-pick c938324823f195a3214681184eb9f34c406e9c74
 
 echo -e $CL_BLU"Cherrypicking Legacy sensors"$CL_RST
 git fetch http://review.cyanogenmod.org/CyanogenMod/android_frameworks_native refs/changes/11/59311/1
@@ -19,8 +18,8 @@ echo -e $CL_BLU"Cherrypicking Core Patch - OMX, reboot/shutdown fix and samsung:
 cp patch/patches/system/core/init/init.c system/core/init/init.c
 cd system/core
 git fetch https://github.com/shine911/AOSPA_system_core kitkat
-git cherry-pick f39215e8ae4166cec8b02593b6e06b523d0e561c
-git cherry-pick 5e4096e52d2f3941006bf530038a147f561a38b4
+git cherry-pick 482266312537d3fb96e762c99d49525508124c25
+git cherry-pick f669e2930b2168180b1316f407b33ba9b5523581
 cd ..
 echo -e $CL_BLU"Cherrypicking vold patch to allow switching storages"$CL_RST
 cd vold
