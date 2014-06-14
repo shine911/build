41710b02102a8d58db1012954b7171a561bc17c2
shine911 <deathbook1999@gmail.com>
2014-06-14 11:33:44 +0700

    SystemUI Crash reduce patch

--- patch.sh
+++ patch.sh
@@ -17,9 +17,11 @@ echo -e $CL_BLU"Cherrypicking Legacy sensors"$CL_RST
 git fetch http://review.cyanogenmod.org/CyanogenMod/android_frameworks_native refs/changes/11/59311/1
 git cherry-pick FETCH_HEAD
 cd ..
-echo -e $CL_BLU"Cherrypicking Ok google and workaround API check Patch - android_frameworks_base"$CL_RST
+echo -e $CL_BLU"Cherrypicking Ok google, workaround API check Patch and SystemUI Crash reduce patch - android_frameworks_base"$CL_RST
 cd base
 patch -p1 -N -i$BASEDIR/patch/patches/frameworks_base.patch
+git fetch http://review.cyanogenmod.org/CyanogenMod/android_frameworks_base refs/changes/34/63034/2
+git cherry-pick FETCH_HEAD
 cd ../..
 echo -e $CL_BLU"Cherrypicking Core Patch - OMX, reboot/shutdown fix"$CL_RST
 cd system/core
