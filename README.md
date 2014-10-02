0e6f94aa0f119b6a95b440a17ad19aa3d1d3105b
Qui Huynh <deathbook1999@gmail.com>
2014-10-02 18:47:51 +0700

    make it's common build

--- README.md
+++ README.md
@@ -2,6 +2,4 @@ Buildbot for building AOSPA.
 =====
 To building:
 
-		./build.sh <device> <REPO SYNC> <CLEAN>
-
-<REPO SYNC> <CLEAN> : use 1 is enable or 0 is disable
+		./build.sh <device>
--- build.sh
+++ build.sh
@@ -10,21 +10,9 @@ bldblu=${txtbld}$(tput setaf 4) # blue
 txtrst=$(tput sgr0) # Reset
 
 DEVICE="$1"
-SYNC="$2"
-CLEAN="$3"
 
 # Time of build startup
 res1=$(date +%s.%N)
-
-# Sync with latest sources
-if [ "$SYNC" == "1" ]
-then
-   echo -e "${bldblu}Syncing latest sources ${txtrst}"
-   repo sync -f
-   echo -e "${bldblu}Starting Patching...${txtrst}"
-   ./patch.sh
-   echo -e "${bldblu}DONE!${txtrst}"	
-fi
 
 # Setup environment
 echo -e "${bldblu}Setting up build environment ${txtrst}"
@@ -37,15 +25,6 @@ prebuilts/misc/linux-x86/ccache/ccache -M 50G
 echo -e "${bldblu}Lunching device... ${txtrst}"
 lunch "pa_$DEVICE-userdebug"
 
-# Clean out folder
-if [ "$CLEAN" == "1" ]
-then
-echo -e "${bldblu}Cleaning up out folder ${txtrst}"
-   make clobber;
-else
-echo -e "${bldblu}Skipping out folder cleanup ${txtrst}"
-fi
-
 # Remove previous build info
 echo -e "${bldblu}Removing previous build.prop ${txtrst}"
 rm $OUT/system/build.prop;
@@ -54,29 +33,11 @@ rm $OUT/system/build.prop;
 echo -e "${bldblu}Starting build for $DEVICE ${txtrst}"
 ./rom-build.sh $DEVICE
 
-
-#echo -e "${bldblu}Removing some unneed file... ${txtrst}"
-#rm -rf $OUT/pa_$DEVICE-ota*.zip
-
-#Upload to devhost
-#case $DEVICE in
-#  "codina")
-#    export FOLDER="37263"
-#    ;;
-#  "codinap")
-#    export FOLDER="38117"
-#    ;;
-#  *)
-#    echo -e "Device upload not supported."
-#    echo -e "Upload skipped!"
 # Get elapsed time
 res2=$(date +%s.%N)
 echo "${bldgrn}Total time elapsed: ${txtrst}${grn}$(echo "($res2 - $res1) / 60"|bc ) minutes ($(echo "$res2 - $res1"|bc ) seconds) ${txtrst}"
     exit 0
 esac
-
-#echo -e "${bldblu}Uploading to DH for $DEVICE ${txtrst}"
-#devhost upload -u $DH_USER -p $DH_PASSWORD out/target/product/$DEVICE/pa_$DEVICE-*.zip -f $FOLDER -d "None" -pb 1
 
 # Get elapsed time
 res2=$(date +%s.%N)
